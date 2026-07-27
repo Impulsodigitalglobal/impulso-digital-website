34cb4e135aa89aa99a9fb984e837780217fcd221
Impulso Digital <impulsodigitalmex@gmail.com>
2026-07-27 00:32:42 -0700

    Fix Vercel entry page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: { host: '0.0.0.0', port: 4173 },
-  build: { rollupOptions: { input: resolve(import.meta.dirname, 'app.html') } },
-});
+  server: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
+})
